e965e4ad086ce8b8cef074819a61bd3f60c108c7
Seung Ho Jang <shjang1992@gmail.com>
2018-08-07 02:05:37 +0900

    Bot responds on user chatting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import * as slack from '@slack/client';
 
-const web = new slack.WebClient(__env.token);
-web.chat.postMessage({ channel: 'random', text: 'This is from @slack/client package' })
-	.then(res => {
-		console.log(res);
-	})
-	.catch(err => {
+const token = __env.token;
+
+const rtm = new slack.RTMClient(token);
+
+async function run() {
+	try {
+		rtm.addListener("message", res => {
+			const botId = res.bot_id as string;
+			const channelId = res.channel as string;
+			const text = res.text as string;
+
+			// Do not respond on other bot's message
+			if(botId !== undefined) {
+				return;
+			}
+
+			rtm.sendMessage(text, channelId);
+		});
+
+		rtm.start();
+
+		console.log("Bot start");
+	} catch (err) {
 		console.error(err);
-	});
+	}
+}
+
+run();
