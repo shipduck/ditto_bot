5a37e860f81c6732c1a58d523ca747cf98d3098b
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-08-07 09:34:44 +0900

    wiki parsing added

--- src/ditto_bot.ts
+++ src/ditto_bot.ts
@@ -1,4 +1,7 @@
 import * as slack from '@slack/client';
+import fetch from "node-fetch";
+
+import * as url from "url";
 
 class Message {
     type: string;
@@ -22,17 +25,16 @@ export class DittoBot {
     run() {
         const rtm = this.rtm;
 
-        try {
-            rtm.addListener("message", res => {
+        rtm.addListener("message", res => {
+            try {
                 this.onMessage(res);
-            });
+            } catch (err) {
+                console.error(err);
+            }
+        });
 
-            rtm.start();
-
-            console.log("Bot start");
-        } catch (err) {
-            console.error(err);
-        }
+        rtm.start();
+        console.log("Bot start");
     }
 
     private onMessage(message: Message) {
@@ -43,6 +45,33 @@ export class DittoBot {
             return;
         }
 
-        this.rtm.sendMessage(message.text, message.channel);
+        const text = message.text;
+        const channel = message.channel;
+
+        if (text.startsWith("<") && text.endsWith(">")) {
+            this.onLink(text.slice(0, text.length - 1).slice(1), channel);
+        }
+        else {
+            // this.rtm.sendMessage(text, channel);
+        }
+    }
+
+    private onLink(link: string, channel: string) {
+        const linkObj = url.parse(link);
+        console.log(linkObj);
+
+        if (linkObj.host === "namu.wiki") {
+            const titleRegex = /<title>(.+) - 나무위키<\/title>/;
+
+            fetch(link)
+                .then(res => res.text())
+                .then(body => {
+                    const match = body.match(titleRegex);
+                    if (match !== null) {
+                        const title = match[1];
+                        this.rtm.sendMessage(title, channel);
+                    }
+                });
+        }
     }
 }
