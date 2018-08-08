d8c77100dbaa44a2bf6d39a50ba50225cad5d124
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-08-08 14:19:35 +0900

    Unfurl links and medias

--- src/ditto_bot.ts
+++ src/ditto_bot.ts
@@ -53,23 +53,23 @@ export class DittoBot {
 
 	private onMessage(message: Message) {
 		// Do not respond on other bot's message
-		if(message.bot_id !== undefined) {
+		if (message.bot_id !== undefined) {
 			return;
 		}
 
 		const text = message.text;
 		const channel = message.channel;
 
-		if(__dev) {
+		if (__dev) {
 			console.log(message);
 		}
 
-		if(text === undefined) {
+		if (text === undefined) {
 			return;
 		}
 
 		const links = matchLinks(text);
-		if(links.length > 0) {
+		if (links.length > 0) {
 			links.forEach((link) => {
 				this.onLink(link, channel);
 			});
@@ -82,19 +82,19 @@ export class DittoBot {
 	private async onLink(link: string, channel: string) {
 		const linkObj = url.parse(link);
 
-		if(linkObj.host === 'namu.wiki') {
+		if (linkObj.host === 'namu.wiki') {
 			try {
 				const res = await sendRequest(link, MethodType.GET, ResultType.TEXT);
 
 				const title = matchNamuWikiTitle(res);
 
-				if(title === null) {
+				if (title === null) {
 					return;
 				}
 
 				this.sendMessage(`<${link}|${title} - 나무위키>`, channel);
 			}
-			catch(err) {
+			catch (err) {
 				console.log(err);
 			}
 		}
@@ -103,27 +103,27 @@ export class DittoBot {
 	private checkText(text: string): string {
 		const retList: string[] = [];
 
-		if(this.keywordExists(text, ['ㄷㄷ', 'ㄷㄷ가마루', '도도가마루'])) {
+		if (this.keywordExists(text, ['ㄷㄷ', 'ㄷㄷ가마루', '도도가마루'])) {
 			retList.push('https://poolc.slack.com/files/U0HJ454UA/FC2KEA249/image.png');
 		}
 
-		if(this.keywordExists(text, ['ㅊㅊ', '추천'])) {
+		if (this.keywordExists(text, ['ㅊㅊ', '추천'])) {
 			retList.push('https://files.slack.com/files-pri/T024R0JEB-FC2EN6MEC/image.png');
 		}
 
-		if(this.keywordExists(text, ['ㅈㄹ', '지랄'])) {
+		if (this.keywordExists(text, ['ㅈㄹ', '지랄'])) {
 			retList.push('https://files.slack.com/files-pri/T024R0JEB-FC3ADCTFX/image.png');
 		}
 
-		if(this.keywordExists(text, ['ㄹㅇ'])) {
+		if (this.keywordExists(text, ['ㄹㅇ'])) {
 			retList.push('https://files.slack.com/files-pri/T024R0JEB-FC4FMUDF0/image.png');
 		}
 
-		if(this.keywordExists(text, ['오도가론'])) {
+		if (this.keywordExists(text, ['오도가론'])) {
 			retList.push('https://poolc.slack.com/messages/C024R0JEP/');
 		}
 
-		if(retList.length === 0) {
+		if (retList.length === 0) {
 			return null;
 		}
 		else {
@@ -147,12 +147,14 @@ export class DittoBot {
 	}
 
 	private sendMessage(text: string, channel: string) {
-		if(__dev) {
+		if (__dev) {
 			text = `${text} : dev`;
 		}
 		this.web.chat.postMessage({
 			'channel': channel,
 			'text': text,
+			'unfurl_media': true,
+			'unfurl_links': true,
 		});
 	}
 }
